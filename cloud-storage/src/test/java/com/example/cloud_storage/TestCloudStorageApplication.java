8940dffa62eccf3075d9d33afb3c0b3a5fc49869
Pavel Shustikov <pavelsh28@gmail.com>
2025-06-08 02:21:47 +0300

    initial commit

---
+++ cloud-storage/src/test/java/com/example/cloud_storage/TestCloudStorageApplication.java
@@ -0,0 +1,11 @@
+package com.example.cloud_storage;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestCloudStorageApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(CloudStorageApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
---
+++ cloud-storage/src/test/java/com/example/cloud_storage/TestcontainersConfiguration.java
@@ -0,0 +1,8 @@
+package com.example.cloud_storage;
+
+import org.springframework.boot.test.context.TestConfiguration;
+
+@TestConfiguration(proxyBeanMethods = false)
+class TestcontainersConfiguration {
+
+}
